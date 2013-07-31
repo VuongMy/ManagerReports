796609654d3115fc5205dba49f3400e6cc2485d3
Vuong My <vuongmy91@gmail.com>
2013-07-31 13:27:21 +0700

    resource

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,27 @@ describe "UserPages" do
     it {should have_title('Sign up page')}
     it {should have_content('Sign up')}
   end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Submit" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do        
+        fill_in "Email",        with: "user@example.com"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
