f12133a861eb5832043f96b1b84daf1001f6530d
abc <abc@abc-Compaq-510.(none)>
2013-08-10 12:14:12 +0700

    modify

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,27 +1,22 @@
-  	require 'securerandom'
+require 'securerandom'
 
-  	class UserMailer < ActionMailer::Base  
-      default :from => "ngoduytrung2901@gmail.com"  
-      
-      def registration_confirmation(user)
-        
-        mail(:to => user.email,:subject => "Registered",
-        	:body => ("email: #{user.email}\n pass: #{user.password} \n#{user.password_digest}"))
-      end
+class UserMailer < ActionMailer::Base  
+  default :from => "framgia.jp@gmail.com"  
+  
+  def registration_confirmation(user)
+    @user=user
+    mail(:to => user.email,:subject => "Registered")
+  end
 
-      def activation(user)
-      	@user = user
-      	mail(:to => "phundk54@gmail.com", :subject => "test")#, :body => "#{Digest::MD5::hexdigest(@user.email)}")
-      end 
+  def activation(user)
+  	@user = user
+  	mail(:to => "framgia.jp@gmail.com", :subject => "Have a account needs to be actived")
+  end 
 
-      def send_report(manager,user)
-        @user = user
-        @manager = manager
-        time = Time.new
-        mail(:to => @manager.email, :subject =>"[#{@user.email}][Daily report]#{time.year}-#{time.month}-#{time.day}")
-      end
-
-      def send_user
-      	mail(:to => "ngoduytrung2901@gmail.com", :subject => "test1")
-      end  
-    end  
+  def send_report(manager,user)
+    @user = user
+    @manager = manager
+    time = Time.new
+    mail(:to => @manager.email, :subject =>"[#{@user.email}][Daily report]#{time.year}-#{time.month}-#{time.day}")
+  end
+end  
